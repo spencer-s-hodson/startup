2a7a5bcca7061e4eff32f8d0a667c7c6e3ac9ee4
Spencer Hodson <spencer.s.hodson@gmail.com>
2023-05-25 18:56:41 -0600

    added the login js

---
+++ login.js
@@ -0,0 +1,16 @@
+function login() {
+  const emailEl = document.getElementById("email");
+  const passEl = document.getElementById("password");
+  localStorage.setItem("email", emailEl.value);
+  localStorage.setItem("password", passEl.value);
+  window.location.href = "home.html";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // On the home page, retrieve the stored email from local storage
+  const storedEmail = localStorage.getItem('email');
+
+  // Display the email on the home page
+  const greetingElement = document.getElementById('welcome_home');
+  greetingElement.textContent = 'Hello, ' + storedEmail;
+});
