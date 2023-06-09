2f51fcfcd0ce5ee01570d3517c35f19a76a556da
Spencer Hodson <spencer.s.hodson@gmail.com>
2023-06-08 23:09:10 -0600

    added a review page that doesn't have any js yet

--- Public/login.js
+++ Public/login.js
@@ -1,9 +1,19 @@
 // This function stores the entered email and password into local storage, and directs the user to the home page
-function login() {
+async function login() {
   const emailEl = document.getElementById("email");
   const passEl = document.getElementById("password");
   localStorage.setItem("email", emailEl.value);
   localStorage.setItem("password", passEl.value);
+  const send_to_server = await fetch("/test", {
+    method: "post",
+    headers: {'Content-type': 'application/json'},
+    body: JSON.stringify({email: emailEl.value, password: passEl.value})
+  })
+
+  const receive_from_server = await send_to_server.json();
+  console.log(send_to_server.status)
+
+
 
   if (!emailEl.value || !passEl.value) {
     event.preventDefault();
